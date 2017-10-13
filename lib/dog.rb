a20f271f73a4dfea90b56672376e8e751f03f077
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-13 19:06:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,6 @@ class Dog
   def initialize(dog_name)
     @name = dog_name
   end
-  def breed=(dog_breed)
-    @breed = dog_breed
-  end
-  def breed
-    @breed
-  end
 end
 
 fido = Dog.new("Fido")
